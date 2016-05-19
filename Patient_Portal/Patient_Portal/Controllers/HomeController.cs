62419da4c97a17b493853f9976d27b23bae4c013
William Hickok <William Hickok>
2016-05-18 20:38:25 -0500

    Displaying all users to Index. Working on Detail view by joining tables together. Getting error with the Ailment Table only.

--- Patient_Portal/Patient_Portal/Controllers/HomeController.cs
+++ Patient_Portal/Patient_Portal/Controllers/HomeController.cs
@@ -1,4 +1,5 @@
 ﻿using Patient_Portal.Models;
+using Patient_Portal.ViewModels;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -10,11 +11,7 @@ namespace Patient_Portal.Controllers
     public class HomeController : Controller
     {
         // Set up the Db context to the database
-        private Patient_PortalDbContext _context;
-        public HomeController(Patient_PortalDbContext context)
-        {
-            _context = context;
-        }
+        private Patient_PortalDbContext _context = new Patient_PortalDbContext();
 
         // GET: Home
         public ActionResult Index()
@@ -27,6 +24,42 @@ namespace Patient_Portal.Controllers
 
             // Pass the patients to the view
             return View(patients.ToList());
+        }
+
+        // Detail View that accepts the id for the patient in the argument
+        public ActionResult Details(int patientId)
+        {
+            // Get a list of all the ailments on the selected patient
+            var patientAilments = (from a in _context.Ailment
+                                   join ap in _context.AilmentPerscription
+                                   on a.IdAilment equals ap.IdAilment
+
+                                   // Filter data by the patient clicked on
+                                   where ap.IdPatientProfile == patientId
+
+                                   select new PatientAilmentDetailViewModel
+                                   {
+                                       AilmentName = a.AilmentName,
+                                   }).ToList();
+
+
+            // Get a list of all the Medications
+            var patientMeds = (from m in _context.Medication
+                                   join mp in _context.MedPerscription
+                                   on m.IdMedication equals mp.IdMedication
+
+                                   // Filter data by the patient clicked on
+                                   where mp.IdPatientProfile == patientId
+
+                                   select new PatientMedDetailVewModel
+                                   {
+                                        MedicationPrice = m.MedicationPrice,
+                                        MedicationName = m.MedicationName,
+                                        MedicationDescription = m.MedicationDescription
+                                   }).ToList();
+
+            return View(patientMeds);
+            
         }
     }
 }
--- Patient_Portal/Patient_Portal/Models/Patient_PortalDbContext.cs
+++ Patient_Portal/Patient_Portal/Models/Patient_PortalDbContext.cs
@@ -8,10 +8,44 @@ namespace Patient_Portal.Models
 {
     public class Patient_PortalDbContext : DbContext
     {
+        public DbSet<PatientProfile> PatientProfile { get; set; }
+        public DbSet<Ailment> Ailment { get; set; }
+        public DbSet<AilmentPerscription> AilmentPerscription { get; set; }
+        public DbSet<Medication> Medication { get; set; }
+        public DbSet<MedPerscription> MedPerscription { get; set; }
+        public DbSet<Procedure> Procedure { get; set; }
+        public DbSet<ProcedurePrescription> ProcedurePrescription { get; set; }
+
         protected override void OnModelCreating(DbModelBuilder modelBuilder)
         {
-            base.OnModelCreating(modelBuilder);
+            modelBuilder.Entity<PatientProfile>()
+                .ToTable("PatientProfile")
+                .HasKey(pp => pp.IdPatientProfile);
+
+            modelBuilder.Entity<Ailment>()
+                .ToTable("Department")
+                .HasKey(a => a.IdAilment);
+
+            modelBuilder.Entity<AilmentPerscription>()
+                .ToTable("AilmentPerscription")
+                .HasKey(ap => ap.IdAilmentPerscription);
+
+            modelBuilder.Entity<Medication>()
+                .ToTable("Medication")
+                .HasKey(m => m.IdMedication);
+
+            modelBuilder.Entity<MedPerscription>()
+                .ToTable("MedPerscription")
+                .HasKey(mp => mp.IdMedPerscription);
+
+            modelBuilder.Entity<Procedure>()
+                .ToTable("Procedure")
+                .HasKey(p => p.IdProcedure);
+
+            modelBuilder.Entity<ProcedurePrescription>()
+                .ToTable("ProcedurePrescription")
+                .HasKey(ppres => ppres.IdProcedurePrescription);
+
         }
-        public DbSet<PatientProfile> PatientProfile { get; set; }
     }
 }
